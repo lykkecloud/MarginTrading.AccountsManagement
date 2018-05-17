815ac56d19a5496dbc05caf989c8fef31bfca529
mihail.yartsev <mihail.yartsev@gmail.com>
2018-05-17 17:28:51 +0300

    MTC-116 Integrational tests fixing

--- src/MarginTrading.AccountsManagement/Workflow/UpdateBalance/UpdateBalanceCommandsHandler.cs
+++ src/MarginTrading.AccountsManagement/Workflow/UpdateBalance/UpdateBalanceCommandsHandler.cs
@@ -66,7 +66,7 @@ namespace MarginTrading.AccountsManagement.Workflow.UpdateBalance
         private async Task<CommandHandlingResult> Handle(UpdateBalanceInternalCommand command,
             IEventPublisher publisher)
         {
-            await _operationStatesRepository.TryInsertOrModifyAsync(OperationName, command.OperationId,
+                await _operationStatesRepository.TryInsertOrModifyAsync(OperationName, command.OperationId,
                 async oldState =>
                 {
                     if (oldState != States.Received.ToString())
