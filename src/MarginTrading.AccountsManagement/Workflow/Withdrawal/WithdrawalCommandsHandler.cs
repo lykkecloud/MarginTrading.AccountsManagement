adfa7113460c36bddc3a2d8d1ff591d1f6cf06fb
val martinchik <valmartsin@gmail.com>
2018-07-24 15:15:04 +0300

    MTC-228 adding saving of the event parameters in the even handler

--- src/MarginTrading.AccountsManagement/Workflow/Withdrawal/WithdrawalCommandsHandler.cs
+++ src/MarginTrading.AccountsManagement/Workflow/Withdrawal/WithdrawalCommandsHandler.cs
@@ -5,6 +5,7 @@ using Lykke.Cqrs;
 using MarginTrading.AccountsManagement.Contracts.Commands;
 using MarginTrading.AccountsManagement.Contracts.Events;
 using MarginTrading.AccountsManagement.Infrastructure;
+using MarginTrading.AccountsManagement.InternalModels;
 using MarginTrading.AccountsManagement.InternalModels.Interfaces;
 using MarginTrading.AccountsManagement.Repositories;
 using MarginTrading.AccountsManagement.Workflow.Withdrawal.Commands;
@@ -19,6 +20,7 @@ namespace MarginTrading.AccountsManagement.Workflow.Withdrawal
         private readonly ISystemClock _systemClock;
         private readonly IAccountsRepository _accountsRepository;
         private readonly IOperationExecutionInfoRepository _executionInfoRepository;
+        private const string OperationName = "Withdraw";
 
         public WithdrawalCommandsHandler(IOperationExecutionInfoRepository executionInfoRepository, 
         ISystemClock systemClock,
@@ -35,21 +37,45 @@ namespace MarginTrading.AccountsManagement.Workflow.Withdrawal
         [UsedImplicitly]
         private void Handle(WithdrawCommand command, IEventPublisher publisher)
         {
-            var executionInfo = new IOperationExecutionInfo<WithdrawalSaga.DepositData>
-            {
-                operationName: command.OperationId,
-                operationId: command.OperationId
-            };
+            
             var account = _accountsRepository.GetAsync(command.ClientId, command.AccountId).GetAwaiter().GetResult();
             if (account == null || account.Balance < command.Amount)
             {
-                await _executionInfoRepository.Save(executionInfo);
+                var executionInfoForFailedEvent =
+                    new OperationExecutionInfo<WithdrawalSaga.DepositData>(OperationName, command.OperationId,
+                        new WithdrawalSaga.DepositData()
+                        {
+                            ClientId = command.ClientId,
+                            AccountId = command.AccountId,
+                            Amount = command.Amount,
+                            AuditLog = command.AuditLog,
+                            Comment = command.Comment,
+                            State = WithdrawalSaga.State.Failed
+
+                        });
+
+                _executionInfoRepository.Save(executionInfoForFailedEvent);
+
                 publisher.PublishEvent(new WithdrawalStartFailedInternalEvent(command.OperationId,
                     _systemClock.UtcNow.UtcDateTime, command.ClientId, command.AccountId, command.Amount,
                     "Not enough balance for withdrawal"));
                 return;
             }
-            
+            var executionInfo =
+                new OperationExecutionInfo<WithdrawalSaga.DepositData>(OperationName, command.OperationId,
+                    new WithdrawalSaga.DepositData()
+                    {
+                        ClientId = command.ClientId,
+                        AccountId = command.AccountId,
+                        Amount = command.Amount,
+                        AuditLog = command.AuditLog,
+                        Comment = command.Comment,
+                        State = WithdrawalSaga.State.FreezingAmount
+
+                    });
+
+            _executionInfoRepository.Save(executionInfo);
+
             publisher.PublishEvent(new WithdrawalStartedInternalEvent(command.OperationId, 
                 _systemClock.UtcNow.UtcDateTime, command.ClientId, command.AccountId, command.Amount, command.Comment, 
                 command.AuditLog));
