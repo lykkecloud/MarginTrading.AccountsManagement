dfee66ae388ea86e9e061cfeae34771b8ba08047
Alexey Andreev <alexey.andreev@lykke.com>
2018-10-14 19:27:04 +0300

    MTC-388: default WithdrawalIsEnabled setting added

--- src/MarginTrading.AccountsManagement/Settings/BehaviorSettings.cs
+++ src/MarginTrading.AccountsManagement/Settings/BehaviorSettings.cs
@@ -6,13 +6,12 @@ namespace MarginTrading.AccountsManagement.Settings
     [UsedImplicitly]
     public class BehaviorSettings
     {
-        [Optional]
-        public string AccountIdPrefix { get; set; }
-        
-        [Optional]
-        public decimal DefaultBalance { get; set; }
-        
-        [Optional]
-        public bool BalanceResetIsEnabled { get; set; }
+        [Optional] public string AccountIdPrefix { get; set; }
+
+        [Optional] public decimal DefaultBalance { get; set; }
+
+        [Optional] public bool BalanceResetIsEnabled { get; set; }
+
+        [Optional] public bool DefaultWithdrawalIsEnabled { get; set; } = false;
     }
 }
