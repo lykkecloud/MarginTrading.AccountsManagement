f4d7da855722a989e7565ef055b0c743d6a15437
Andrey Anokhin <andrey.anokhin@lykke.com>
2019-02-04 03:01:41 +0300

    MTC-485: fixed typo

--- src/MarginTrading.AccountsManagement/Extensions/ConfigurationRootExtensions.cs
+++ src/MarginTrading.AccountsManagement/Extensions/ConfigurationRootExtensions.cs
@@ -4,10 +4,10 @@ namespace MarginTrading.AccountsManagement.Extensions
 {
     public static class ConfigurationRootExtensions
     {
-        public static bool NotTrowExceptionsOnServiceValidation(this IConfigurationRoot configuration)
+        public static bool NotThrowExceptionsOnServiceValidation(this IConfigurationRoot configuration)
         {
-            return !string.IsNullOrEmpty(configuration["NOT_TROW_EXCEPTIONS_ON_SERVICES_VALIDATION"]) &&
-                   bool.TryParse(configuration["NOT_TROW_EXCEPTIONS_ON_SERVICES_VALIDATION"],
+            return !string.IsNullOrEmpty(configuration["NOT_THROW_EXCEPTIONS_ON_SERVICES_VALIDATION"]) &&
+                   bool.TryParse(configuration["NOT_THROW_EXCEPTIONS_ON_SERVICES_VALIDATION"],
                        out var trowExceptionsOnInvalidService) && trowExceptionsOnInvalidService;
         }
     }
