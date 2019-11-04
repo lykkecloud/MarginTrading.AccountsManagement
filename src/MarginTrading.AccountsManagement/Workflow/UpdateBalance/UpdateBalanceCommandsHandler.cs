d4dd262e6fe6df9607913f6add96d34ec239dbac
Aliaksandr Vashetsin <vashetsin@gmail.com>
2019-11-04 09:33:08 +0100

    LT-1786: Smart capital figures update: Balance is not updated on commission when position is closed

--- src/MarginTrading.AccountsManagement/Workflow/UpdateBalance/UpdateBalanceCommandsHandler.cs
+++ src/MarginTrading.AccountsManagement/Workflow/UpdateBalance/UpdateBalanceCommandsHandler.cs
@@ -103,6 +103,8 @@ namespace MarginTrading.AccountsManagement.Workflow.UpdateBalance
                     tradingDate: command.TradingDay);
 
                 var convertedAccount = Convert(account);
+                
+                await _executionInfoRepository.SaveAsync(executionInfo);
 
                 publisher.PublishEvent(
                     new AccountChangedEvent(
@@ -113,8 +115,6 @@ namespace MarginTrading.AccountsManagement.Workflow.UpdateBalance
                         change,
                         command.OperationId)
                 );
-                
-                await _executionInfoRepository.SaveAsync(executionInfo);
             }
         }
 
