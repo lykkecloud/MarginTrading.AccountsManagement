9554b5b00b4c3b4f44f46fda04b5ccc63fdf93b3
Andrei Tarutin <tarutin_andrew@mail.ru>
2020-07-30 13:00:39 +0300

    replaced regular subscribers with cqrs projections

--- src/MarginTrading.AccountsManagement/Startup.cs
+++ src/MarginTrading.AccountsManagement/Startup.cs
@@ -112,7 +112,6 @@ namespace MarginTrading.AccountsManagement
             builder.RegisterModule(new AccountsManagementModule(_mtSettingsManager, Log));
             builder.RegisterModule(new AccountsManagementExternalServicesModule(_mtSettingsManager));
             builder.RegisterModule(new CqrsModule(_mtSettingsManager.CurrentValue.MarginTradingAccountManagement.Cqrs, Log));
-            builder.RegisterModule(new RabbitMqModule(_mtSettingsManager, Log));
         }
 
         [UsedImplicitly]
