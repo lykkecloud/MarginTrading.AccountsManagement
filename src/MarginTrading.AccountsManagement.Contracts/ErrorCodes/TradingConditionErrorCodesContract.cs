c12056a64f53df7fae3830401ddc9c1fedbdd5c0
Gennady Ponomarev <gennady.ponomarev@lykke-business.ch>
2021-03-15 15:35:29 +0500

    LT-3138: refactor trading condition patch api with error codes

---
+++ src/MarginTrading.AccountsManagement.Contracts/ErrorCodes/TradingConditionErrorCodesContract.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2019 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace MarginTrading.AccountsManagement.Contracts.ErrorCodes
+{
+    public enum TradingConditionErrorCodesContract
+    {
+        None,
+        TradingConditionNotFound,
+        ClientHasOpenPositions,
+    }
+}
