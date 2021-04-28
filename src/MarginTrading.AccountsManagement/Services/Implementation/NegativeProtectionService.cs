215750789d59ea5179b2033f2750e6c8af456403
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-04-28 19:38:22 +0300

    rebased upon master

--- src/MarginTrading.AccountsManagement/Services/Implementation/NegativeProtectionService.cs
+++ src/MarginTrading.AccountsManagement/Services/Implementation/NegativeProtectionService.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using static System.Math;
 using System.Threading.Tasks;
 using Common;
