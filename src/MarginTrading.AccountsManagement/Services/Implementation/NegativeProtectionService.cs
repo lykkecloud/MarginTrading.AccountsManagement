7893059d8112525c5c147026772ecd130e50d616
Andrei Tarutin <tarutin_andrew@mail.ru>
2021-04-28 19:24:22 +0300

    adding created at value for auto compensation events

--- src/MarginTrading.AccountsManagement/Services/Implementation/NegativeProtectionService.cs
+++ src/MarginTrading.AccountsManagement/Services/Implementation/NegativeProtectionService.cs
@@ -3,6 +3,7 @@
 
 using static System.Math;
 using System.Threading.Tasks;
+using Common;
 using MarginTrading.AccountsManagement.InternalModels;
 using MarginTrading.AccountsManagement.Settings;
 using MarginTrading.AccountsManagement.Workflow.NegativeProtection;
@@ -37,13 +38,15 @@ namespace MarginTrading.AccountsManagement.Services.Implementation
 
             if (_negativeProtectionAutoCompensation)
             {
+                var auditLog = new {CreatedAt = DateTime.UtcNow};
+                
                 await _sendBalanceCommandsService.ChargeManuallyAsync(
                     accountId,
                     compensationAmount, 
                     $"{operationId}-negative-protection",
                     "Negative protection",
                     NegativeProtectionSaga.CompensationTransactionSource,
-                    null,
+                    auditLog.ToJson(),
                     AccountBalanceChangeReasonType.CompensationPayments,
                     operationId,
                     null,
